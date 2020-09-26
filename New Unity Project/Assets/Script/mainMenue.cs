28915653ef1e851470ceea6ff77bbd75dc59b855
bluej3270 <kellanclapp@gmail.com>
2020-09-25 21:06:08 -0600

    Finished Main Menue

---
+++ New Unity Project/Assets/Script/mainMenue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenue : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void playGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        Debug.Log("Load Scene");
+    }
+    public void quitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
