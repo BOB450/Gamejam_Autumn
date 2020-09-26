08b4841e03c87b33203b90d87b5a0820ba9243cf
bluej3270 <kellanclapp@gmail.com>
2020-09-26 14:10:23 -0600

    Folowed instructions to make broken things

---
+++ New Unity Project/Assets/Script/playerDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerDetection : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void OnTriggerEnter2D(Collider2D colider) {
+        if (colider.gameObject.tag == "player") {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    } 
+}
