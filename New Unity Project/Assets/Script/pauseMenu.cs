ec8bca4ef20d247a398c8ddf2a55c3c601e31919
bluej3270 <kellanclapp@gmail.com>
2020-09-25 23:03:44 -0600

    Added more prefabs and updated map

--- New Unity Project/Assets/Script/pauseMenu.cs
+++ New Unity Project/Assets/Script/pauseMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class puaseMenu : MonoBehaviour
+public class pauseMenu : MonoBehaviour
 {
     public bool gameispuased = false;
     public GameObject UI;
